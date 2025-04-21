c3ed0bda4550f89b6475ae37536dbbb0b7e23459
CamCoder08 <y30281@gmail.com>
2025-04-21 21:42:37 +0900

    [feat] Project 코드베이스 초기 세팅 및 파일분리
    - MVC 구조
    - 코드베이스 설정
    - SnapKit
    - SceneDelegate 수정

---
+++ PokemonContacts/PokemonContacts/Model/Contact.swift
@@ -0,0 +1,8 @@
+//
+//  Contact.swift
+//  PokemonContacts
+//
+//  Created by ByonJoonYoung on 4/21/25.
+//
+
+import Foundation
---
+++ PokemonContacts/PokemonContacts/PokemonListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonContacts
+//
+//  Created by ByonJoonYoung on 4/21/25.
+//
+
+import UIKit
+
+class PokemonListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
