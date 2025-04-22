4578cd4162528b44fcf8740ac28ee6378e07917c
CamCoder08 <y30281@gmail.com>
2025-04-22 16:41:06 +0900

    [refactor]  Model 폴더 내부에 PokemonModel 파일을 생성한 뒤 기존 구조체 코드 이동

--- PokemonContacts/PokemonContacts/Network/PokemonAPI.swift
+++ PokemonContacts/PokemonContacts/Network/PokemonAPI.swift
@@ -7,18 +7,4 @@
 
 import Foundation
 
-struct Pokemon: Decodable {
 
-    let id: Int
-    let name: String
-    let sprites: Sprites
-}
-
-struct Sprites: Decodable {
-
-    let frontDefault: String
-
-    enum CodingKeys: String, CodingKey {
-        case frontDefault = "front_default"
-    }
-}
